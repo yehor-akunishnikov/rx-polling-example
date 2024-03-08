78b43d02f1a3159be5da1ca8a6ea78ce8775a7f2
Reonido <faiteforever@gmail.com>
2024-03-08 02:53:20 +0200

    Simplify settings form value delivery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {AsyncPipe, JsonPipe} from '@angular/common';
 
-import {BehaviorSubject, Observable} from 'rxjs';
+import {map, Observable, startWith} from 'rxjs';
 
 import {ProcessPollingComponent} from 'user-polling';
 import {InitPayload} from 'data-layers';
@@ -22,20 +22,15 @@ export interface SettingsForm {
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public settingsForm: FormGroup<SettingsForm> = new FormGroup({
     id: new FormControl('ID'),
     isSuccessProgress: new FormControl(true),
     canObtainData: new FormControl(true),
   });
 
-  private settingsState = new BehaviorSubject<InitPayload>(this.settingsForm.getRawValue());
-
-  public initPayload$: Observable<InitPayload> = this.settingsState.asObservable();
-
-  public ngOnInit(): void {
-    this.settingsForm.valueChanges.subscribe((initPayload: InitPayload) => {
-      this.settingsState.next(initPayload);
-    });
-  }
+  public inputPayload$: Observable<InitPayload> = this.settingsForm.valueChanges.pipe(
+    map(() => this.settingsForm.getRawValue()),
+    startWith(this.settingsForm.getRawValue()),
+  );
 }
